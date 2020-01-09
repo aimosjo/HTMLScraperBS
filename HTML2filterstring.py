88d1e4da7414549aac5618844edc35bebb49796e
aimosjo <aj.kjohnston@gmail.com>
2020-01-09 16:54:42 +0800

    Added support for sorting the HTML soup by CSS class identifier

--- HTML2filterstring.py
+++ HTML2filterstring.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
+import os
 
-def filterHTML(htmlstring, cssid=None, tag=None):
+# parameters are htmlstring (opened file), cssid is the id class from CSS, tag are the tags ('a', 'p'),
+# and class_ is the CSS class (all found by looking in the HTML doc)
+def filterHTML(htmlstring, cssid=None, tag=None, class_=None):
 	#parse the HTML code using BeautifulSoup, so we can filter the HTML by cssid and tags
-	soup = BeautifulSoup(htmlstring, 'html.parser')
+	soup = BeautifulSoup(open(htmlstring), 'html.parser')
 
 	# store the result of the soup's filtered contents in a string
 	# user will need to inspect webpage and HTML to find appropriate parameters
-	filteredSoup = soup.find_all(tag, id=cssid)
+	filteredSoup = soup.find_all(tag, id=cssid, class_=class_)
 	lines = []
-	print(len(filteredSoup))
+	# print(len(filteredSoup))
 
 	for x in range(0, len(filteredSoup)):
 		lines.append(filteredSoup[x].get_text())
