64db6d79462ade6c1e9194676fca9c5126241bdc
aimosjo <aj.kjohnston@gmail.com>
2020-01-09 21:07:17 +0800

    completed tuple grouping, now returns a list of proper n-tuplets"

--- commastring2listofatt.py
+++ commastring2listofatt.py
@@ -7,25 +7,36 @@ import numpy as np
 # [LATER] implement a check if there is incomplete data or incorrectly categorized data being added
 
 def commaatt(stringofatt, n):
-	# create a string of the split att
+	# create a list of the split att
 	att = stringofatt.split(',')
 
+	# remove any extra blank values
+
+
+	# temporarily blocked for troubleshooting
 	# check if there will be leftover values after
 	# placing the string into a list of strings of length with n attributes
-	
-	## turn this back on!!
-	##if not (np.mod(len(att),n)) == 0:
-	##	print("Check the dimensions of the data.")
-	##	return -1
+	if not (np.mod(len(att),n)) == 0:
+		print("Check the dimensions of the data.")
+		return -1
 
 	# check how many copies of n there are in len(att) [use this number to iterate up to, placing values]
 	# should be int, since len(att)%n == 0 by check above
-	ind = len(att)/n
 
-	# initialize the array
-	att_strings = []
+	# for troubleshooting, going to coerce this into an integer to see where things are going wrong
+	ind = int(len(att)/n)
+
+	# initialize the list
+	att_strings = ['']
 	# use for loop to make list of n-attribute strings
 	for x in range(0,ind):
+		#print("Current value of x: " + str(x))
+		att_strings.append('')
+		
 		for y in range(0,n):
-			# att_strings[x*n] += att[x*n + y]
-			print(x*n + y)
+			#print("Current value of x: " + str(x))
+			#print(att[x*n + y])
+			att_strings[x] += att[x*n + y] + ","
+		#print(att_strings[x])
+	print(list(att_strings)[:-1])
+	return list(att_strings[:-1])
