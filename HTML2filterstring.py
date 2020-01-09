0cb053a1f7ea3c96a44bc2d3feebef40f53d44f4
aimosjo <aj.kjohnston@gmail.com>
2020-01-09 18:11:49 +0800

    Changed lines from list to string, and changed the space between values from ' ' to ',' (comma separated now, for easier parsing)

--- HTML2filterstring.py
+++ HTML2filterstring.py
@@ -10,11 +10,11 @@ def filterHTML(htmlstring, cssid=None, tag=None, class_=None):
 	# store the result of the soup's filtered contents in a string
 	# user will need to inspect webpage and HTML to find appropriate parameters
 	filteredSoup = soup.find_all(tag, id=cssid, class_=class_)
-	lines = []
+	lines = ""
 	# print(len(filteredSoup))
 
 	for x in range(0, len(filteredSoup)):
-		lines.append(filteredSoup[x].get_text())
+		lines += filteredSoup[x].get_text() + ","
 		# print(filteredSoup[x].get_text())
 	#filtSoupType = type(soup.find_all(tag, id=cssid))
 	
