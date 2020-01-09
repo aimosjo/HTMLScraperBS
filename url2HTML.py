f577edc5f97fb00e95698126238cd5e4d3d9a985
aimosjo <aj.kjohnston@gmail.com>
2020-01-09 13:59:17 +0800

    No longer using wget to download HTML as troubles arose with saving the file as an HTML file; requests cut out a lot of code, and has less overhead

--- url2HTML.py
+++ url2HTML.py
@@ -1,17 +1,20 @@
-# need to install wget before importing
-import wget
+# import requests library so user can connect to url
+import requests
 
-# url = input("Enter website's url:")
+#import url2domain script so user can scrape the domain name from the url
+import url2domain
 
 # [LATER] check that user can connect to URL, otherwise return message to try again later
 
 def urlToHTML(url):
-	# use wget to download the HTML text
-	down = wget.download(url)
+	# call url2domain and use function to name the html file appropriately
+	# add time stamp function to this s.t. the saved html file will not be overwritten
+	webname = url2domain.urlToDomain(url)
+	webname += ".html"
 
-	# create a file to hold the opened down file in read only mode
-	with open(down) as f:
-		htmlText = ".\n".join(f.readlines())
+	# use requests library to scrape the HTML text from the website
+	r = requests.get(url)
 
-	# return the necessary HTML text from the webpage as a string
-	return(htmlText)
+	Html_file= open(webname,"w")
+	Html_file.write(r.text)
+	Html_file.close()
